37a9730d65d02ffa0c4df14fbda24689141b5510
Prashant Pal <palprashant465@gmail.com>
2025-03-27 22:40:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function insert_Row() {
-    let tab = document.getElementById('sampleTable') ;
-	let add = document.createElement('tr');
-	add.innerHTML =  `<td>${'New Cell1'}</td>
-	                    <td>${'New Cell2'}</td>`
-	tab.appendChild(add);
-  
+    let tab = document.getElementById('sampleTable');
+    let add = document.createElement('tr');
+    
+    add.innerHTML = `<td>New Cell1</td><td>New Cell2</td>`;
+    
+    tab.insertBefore(add, tab.rows[0]);
 }
