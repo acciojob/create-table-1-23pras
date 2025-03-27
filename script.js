6ba80e1275e3de2925813a88b11fd851969d8405
Prashant Pal <palprashant465@gmail.com>
2025-03-27 22:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function insert_Row() {
     let tab = document.getElementById('sampleTable');
     let add = document.createElement('tr');
-    
+
     add.innerHTML = `<td>New Cell1</td><td>New Cell2</td>`;
-    
-    tab.insertBefore(add, tab.rows[0]);
+
+    if (tab.rows.length > 0) {
+        tab.insertBefore(add, tab.rows[0]); 
+    } else {
+        tab.appendChild(add); 
+    }
 }
